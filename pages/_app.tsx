5782a3d355da757010ca251336f893a8a83141e3
wb-keeper <wb-keeper@mail.ru>
2023-04-19 16:40:41 +0300

    Some more changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from 'next/app'
 import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import {SessionProvider} from "next-auth/react";
+import Toaster from "react-hot-toast";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <>
+      <SessionProvider session={pageProps.session}>
+          <Toaster />
           <RegisterModal />
         <LoginModal />
           <Layout>
           <Component {...pageProps} />
         </Layout>
-      </>
+      </SessionProvider>
 
   )
 
--- pages/api/current.ts
+++ pages/api/current.ts
@@ -7,6 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
     try {
         const { currentUser } = await serverAuth(req)
+        return res.status(200).json(currentUser)
     } catch (error) {
         console.log(error)
         return res.status(400).end()
